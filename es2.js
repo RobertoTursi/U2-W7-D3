f6efa0ffe17a1badaa36413ecb1bf21ceeaf1be5
RobertoTursi <Robertotursi00@gmail.com>
2023-01-25 17:53:47 +0100

    Update es2.js

--- es2.js
+++ es2.js
@@ -1,20 +1,15 @@
 const p = document.getElementById("countdown")
 
 
- counter = sessionStorage.getItem("timePassed")
- const addToSessionStorage = () =>{
+ let counter = sessionStorage.getItem("timePassed") || 0  //questa riga è l'equivalente di scrivere un if dove:
+//  if(sessionStorage.getItem("timePassed") !== null){
+//     let counter = sessionStorage.getItem("timePassed")
+//  } else {
+//     let counter = 0
+//  }
+const addToSessionStorage = () =>{
      sessionStorage.setItem("timePassed", counter)
      counter++
      p.innerText = counter
  }
  setInterval(addToSessionStorage, 1000)
-
-
-
-// let counter = sessionStorage.getItem("count")
-// setInterval(conta, 1000)
-// function conta(){
-//     counter++
-//     p.innerText = counter
-//     sessionStorage.setItem("count", counter)
-// }
